c120160209cf09c6a364b22820804ce74404d254
samratchhetri <sachh21@student.sdu.dk>
2023-12-15 16:08:22 +0100

    Silas

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 Hello World
 <?php
 echo `php!!`;
+
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$database = "myDB";
+
+try{
+$connection = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+echo "Database connected - YAY!";
+
+}catch (PDOexception $exception){
+    echo "database not connected, " , $exception;}
