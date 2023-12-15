0529a581cc860725348ec1e8cbe57f412d61e6a6
samratchhetri <sachh21@student.sdu.dk>
2023-12-15 20:15:56 +0100

    Setup

--- index.php
+++ index.php
@@ -11,15 +11,33 @@ $database = "myDB";
 
 try{
 $connection = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
-echo "Database connected - YAY!";
 
 }catch (PDOexception $exception){
     echo "database not connected, " , $exception;}
 ?>
 
     <form method="Post">
-        <textarea name="sql" placeholder="WRITE SQL CODE HERE!">Ferrari no good</textarea>
+        <textarea name="sql" placeholder="WRITE SQL CODE HERE!">
+Team	       Date	        Location
+Haas	       January 31	Online
+Redbull        Feburary 03      New York
+Williams       Feburary 06      Online
+Alfa Romeo     Feburary 07      Zurich
+AlphaTauri     Feburary 11      New York
+Aston Martin   Feburary 13      Silverstone
+McLaren        Feburary 13      McLaren Technology Centre
+Ferrari        Feburary 14      Maranello
+Mercedes       Feburary 15      Silverstone
+Alpine         Feburary 16      London
+
+</textarea>
         <br>
         <input type="submit">
 </form>
+<form method="Post">
+        <textarea name="sql" placeholder="WRITE SQL CODE HERE!">Haas	January 31	Online</textarea>
+        <br>
+        <input type="submit">
+        </form>
+
 
